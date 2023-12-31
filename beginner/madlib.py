bdf47718e3231f380356cd3867a45f25a5885328
Muhammad Ahmod <53302444+Muhammad-1990@users.noreply.github.com>
2023-12-31 17:13:25 +0000

    Implemented a simple madlib program that prompts user for input and generates a story

---
+++ beginner/madlib.py
@@ -0,0 +1,19 @@
+# A simple madlib program that asks the user for input and then prints out a story.
+
+# Ask the user for input
+print("Please enter a noun:")
+noun = input()
+
+print("Please enter a verb:")
+verb = input()
+
+print("Please enter an adjective:")
+adjective = input()
+
+print("Please enter an adverb:")
+adverb = input()
+
+# Print out the story
+print(f"Do you {verb} your {adjective} {noun} {adverb}? That's hilarious!")
+
+# End of program
